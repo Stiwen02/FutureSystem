bb1277bdb9688ba4a912a21d8b6ae0a0962addaa
Stiwen02 <146366486+Stiwen02@users.noreply.github.com>
2023-10-18 18:29:48 +0200

    Changes

--- FutureSystem/main.py
+++ FutureSystem/main.py
@@ -1,88 +1,62 @@
 # import traceback
 from future import *
+import gc
 
-try:
-	# Import functionality
-	modules = []
+# Import functionality
+modules = []
 
-	def import_module(name, to_globals=False):
-		global modules
+def import_module(name, to_globals=False):
+	global modules
 
-		name = name
-		filename = "modules/" + name + ".py"
+	name = name
+	filename = "modules/" + name + ".py"
 
-		if filename in modules:
-			raise Exception("Module '" + filename + "' already imported")
+	if filename in modules:
+		raise Exception("Module '" + filename + "' already imported")
 
-		with open(filename, "r") as file:
-			code = file.read()
-		bytecodes = compile(code, filename, "exec")
+	with open(filename, "r") as file:
+		code = file.read()
+	bytecodes = compile(code, filename, "exec")
 
-		exec_locals = {}
-		exec(bytecodes, exec_locals)
-		
-		if to_globals:
-			exec(bytecodes, globals())
-
-		module_class = type(name, (), exec_locals)
-		del exec_locals
-
-		modules.append(filename)
-		return module_class
-
-	# Import
-	import_module("keyboard", True)
-	import_module("file_manager", True)
-	import_module("list", True)
-	import_module("utils", True)
-	import_module("info", True)
-	import_module("display_info", True)
-	import_module("buttons", True)
-	import_module("user_input", True)
-	import_module("screen", True)
-	import_module("bitmap", True)
-	import_module("bluetooth", True)
-	import_module("buzzer", True)
-	import_module("data", True)
-	import_module("execute", True)
-	import_module("game_engine", True)
-	import_module("text", True)
-	import_module("wifi", True)
-
-	# Main
-	screen.sync = 0
+	exec_locals = {}
+	exec(bytecodes, exec_locals)
 	
-	name = keyboard("What's your name?")
-	notify("Hello, " + name + ".")
-except Exception as error:
-	from future import *
-	
-	screen_width = 160
-	screen_height = 128
+	if to_globals:
+		exec(bytecodes, globals())
 
-	character_width = 8
-	character_height = 16
+	module_class = type(name, (), exec_locals)
+	del exec_locals
+	del name
+	del code
+	del bytecodes
+	gc.mem_free()
+	gc.mem_alloc()
 
-	new_line_offset = 2
+	modules.append(filename)
+	del filename
+	return module_class
 
-	character_full_height = character_height + new_line_offset
-	max_horizontal_characters = screen_width // character_width
+# Import
+import_module("keyboard", True)
+import_module("file_manager", True)
+import_module("list", True)
+import_module("utils", True)
+import_module("info", True)
+import_module("display_info", True)
+import_module("buttons", True)
+import_module("user_input", True)
+import_module("screen", True)
+import_module("bitmap", True)
+import_module("bluetooth", True)
+import_module("buzzer", True)
+import_module("data", True)
+import_module("execute", True)
+import_module("game_engine", True)
+import_module("text", True)
+import_module("wifi", True)
 
-	def wrap_text_d(text):
-		words = text.split()
-		new_lines = []
-		line = ""
+# Main
+screen.sync = 0
 
-		for word in words:
-			if len(line + word) + len(line.split()) <= max_horizontal_characters:
-				line += ("" if line == "" else " ") + word
-			else:
-				new_lines.append(line)
-				line = word
-		new_lines.append(line)
-		return "\n".join(new_lines)
-
-	screen.text(wrap_text_d(repr(error)))
-	# traceback.print_exc()
-	while True:
-		pass
+name = keyboard("What's your name?")
+notify("Hello, " + name + ".")
